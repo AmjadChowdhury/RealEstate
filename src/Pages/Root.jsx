ae69047ea7cf971420eada1536a990329dd0be45
AmjadChowdhury <amjadchowdhury247@gmail.com>
2024-10-16 14:59:42 +0100

    Navbar & slider added

--- src/Pages/Root.jsx
+++ src/Pages/Root.jsx
@@ -1,9 +1,13 @@
 import { Outlet } from "react-router-dom";
+import Navbar from "../Shared/Navbar";
+import Slider from "../Shared/Slider";
 
 
 const Root = () => {
     return (
         <div>
+            <Navbar></Navbar>
+            <Slider></Slider>
             <Outlet></Outlet>
         </div>
     );

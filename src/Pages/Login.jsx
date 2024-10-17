77765d41c37283d4aefdc650948989cdf425cc82
AmjadChowdhury <amjadchowdhury247@gmail.com>
2024-10-17 06:24:59 +0100

    Set some condition before login & register

--- src/Pages/Login.jsx
+++ src/Pages/Login.jsx
@@ -1,8 +1,10 @@
-import { useContext } from "react";
+import { useContext, useState } from "react";
 import { AuthContext } from "../Provider/AuthProvider";
 import { Link, useLocation, useNavigate } from "react-router-dom";
 
 const Login = () => {
+    const [successLogin,setSuccessLogin] = useState('')
+    const [errorLogin,setErrorLogin] = useState('')
     const { signIn } = useContext(AuthContext)
     const navigate = useNavigate()
     const location = useLocation()
@@ -12,13 +14,30 @@ const Login = () => {
         const email = e.target.email.value
         const password = e.target.password.value
         // console.log(email,password)
+        // form reset..
+        e.target.reset()
+
+        setSuccessLogin('')
+        setErrorLogin('')
+
+        if(password.length < 6){
+          setErrorLogin('Password must be greater than six character')
+          return
+        }
+        else if(!/[A-Z]/.test(password)){
+          setErrorLogin('Password must have one uppercase letter')
+          return
+        }
+
         signIn(email,password)
         .then(result => {
             console.log(result.user)
+            setSuccessLogin(`${result.user?.displayName} Login successfully`)
             navigate(location?.state ? location.state : "/")
         })
         .catch(error => {
             console.log(error.message)
+            setErrorLogin(error.message)
         })
         
     }
@@ -29,7 +48,7 @@ const Login = () => {
         <form onSubmit={handleLogIn} className="card-body">
           <div className="form-control">
             <label className="label">
-              <span className="label-text">Email</span>
+              <span className="label-text text-xl font-bold">Email</span>
             </label>
             <input
               name="email"
@@ -41,7 +60,7 @@ const Login = () => {
           </div>
           <div className="form-control">
             <label className="label">
-              <span className="label-text">Password</span>
+              <span className="label-text text-xl font-bold">Password</span>
             </label>
             <input
               name="password"
@@ -51,13 +70,24 @@ const Login = () => {
               required
             />
             <label className="label">
-              <a href="#" className="label-text-alt link link-hover">
+              <a href="#" className="label-text-alt link link-hover text-blue-700 underline">
                 Forgot password?
               </a>
             </label>
           </div>
           <div className="form-control mt-6">
-            <button className="btn bg-blue-700">Login</button>
+            <button className="btn bg-blue-700 font-bold text-white hover:bg-purple-500">Login</button>
+          </div>
+
+          <div>
+            {
+              successLogin && <p className="text-center text-green-700 text-xl font-semibold">{successLogin}</p>
+            }
+          </div>
+          <div>
+            {
+              errorLogin && <p className="text-center text-red-700 text-xl font-semibold">{errorLogin}</p>
+            }
           </div>
         </form>
         <p className="mb-4 text-center text-xl font-semibold">New ? Please <Link className="underline" to="/register">Register</Link></p>
--- src/Pages/Register.jsx
+++ src/Pages/Register.jsx
@@ -1,4 +1,4 @@
-import { useContext } from "react";
+import { useContext, useState } from "react";
 import { AuthContext } from "../Provider/AuthProvider";
 import { Link } from "react-router-dom";
 import { updateProfile } from "firebase/auth";
@@ -7,16 +7,40 @@ import auth from "../Firebase/firebase.config";
 
 const Register = () => {
     const { createUser } = useContext(AuthContext)
+    const [successRegister,setSuccessRegister] = useState('')
+    const [errorRegister,setErrorRegister] = useState('')
     const handleRegister = e => {
         e.preventDefault()
         const email = e.target.email.value
         const password = e.target.password.value
         const name = e.target.name.value
         const photo = e.target.photo.value
-        console.log(email,password)
+        const accepted = e.target.terms.checked
+        console.log(email,password,accepted)
+
+        setSuccessRegister('')
+        setErrorRegister('')
+
+        if(password.length < 6){
+          setErrorRegister('Password must be greater than six character')
+          return
+        }
+        else if(!/[A-Z]/.test(password)){
+          setErrorRegister('Password must have one uppercase letter')
+          return
+        }
+        else if(!accepted){
+          setErrorRegister('Please check terms & condition!!!')
+          return
+        }
+        // claer form..
+        e.target.reset()
+
+
         createUser(email,password)
         .then(result => {
             console.log(result.user)
+            setSuccessRegister(`${result.user?.displayName} created account successfully`)
             updateProfile(auth.currentUser, {
                 displayName : name,
                 photoURL : photo
@@ -26,10 +50,12 @@ const Register = () => {
             })
             .catch(error => {
                 console.log(error.message)
+                setErrorRegister(error.message)
             })
         })
         .catch(error => {
             console.log(error.message)
+            setErrorRegister(error.message)
         })
     }
     return (
@@ -38,7 +64,7 @@ const Register = () => {
         <form onSubmit={handleRegister} className="card-body">
         <div className="form-control">
             <label className="label">
-              <span className="label-text">Name</span>
+              <span className="label-text text-xl font-bold">Name</span>
             </label>
             <input
               name="name"
@@ -50,7 +76,7 @@ const Register = () => {
           </div>
           <div className="form-control">
             <label className="label">
-              <span className="label-text">PhotoURL</span>
+              <span className="label-text text-xl font-bold">PhotoURL</span>
             </label>
             <input
               name="photo"
@@ -62,7 +88,7 @@ const Register = () => {
           </div>
           <div className="form-control">
             <label className="label">
-              <span className="label-text">Email</span>
+              <span className="label-text text-xl font-bold">Email</span>
             </label>
             <input
               name="email"
@@ -74,7 +100,7 @@ const Register = () => {
           </div>
           <div className="form-control">
             <label className="label">
-              <span className="label-text">Password</span>
+              <span className="label-text text-xl font-bold">Password</span>
             </label>
             <input
               name="password"
@@ -83,14 +109,24 @@ const Register = () => {
               className="input input-bordered"
               required
             />
-            <label className="label">
-              <a href="#" className="label-text-alt link link-hover">
-                Forgot password?
-              </a>
-            </label>
+          </div>
+          <div className="flex items-center gap-2">
+            <input type="checkbox" name="terms" id="terms" />
+            <label htmlFor="terms">Check terms & condition</label>
           </div>
           <div className="form-control mt-6">
-            <button className="btn bg-blue-700">Register</button>
+            <button className="btn bg-blue-700 font-bold text-white hover:bg-purple-700">Register</button>
+          </div>
+
+          <div>
+            {
+              successRegister && <p className="text-xl text-center font-bold text-green-700">{successRegister}</p>
+            }
+          </div>
+          <div>
+            {
+              errorRegister && <p className="text-xl text-center font-bold text-red-700">{errorRegister}</p>
+            }
           </div>
         </form>
         <p className="mb-4 text-center text-xl font-semibold">Already have an account? <Link className="underline" to="/login">Login</Link></p>
